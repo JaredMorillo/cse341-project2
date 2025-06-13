3ab677b068bdbc9dfa4b496cdae5580347238606
JaredMorillo <jared.morillo@hotmail.com>
2025-06-13 12:09:57 -0500

    Fixing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 router.use('/products', require('./products'));
 
-router.get('/login', passport.authenticate('github'), (req.res), {});
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', function(req, res, next)  {
     req.logout(function(err) {
--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
 
-
-const port = process.env.PORT || 3000;
 const app = express();
+const port = process.env.PORT || 3000;
 
+//CORS configuration
+app.use(cors({origin: '*',}))
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS']}))
+
+//Body parser configuration
 app.use(bodyParser.json());
+
+// Express session configuration
 app.use(session({
     secret: 'secret',
     resave: false,
@@ -33,21 +41,19 @@ app.use((req, res, next) => {
     );
     next();
 })
-app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS']}))
-app.use(cors({origin: '*',}))
+
+
+// Swagger route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//Routes
 app.use('/', require('./routes/index'));
 
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   callbackURL: process.env.GITHUB_CALLBACK_URL,
-},
-function(accessToken, refreshToken, profile, done) {
-    // This function is called after GitHub authentication
-    // You can save the user profile to your database here
-    //User.findOrCreate({ githubId: profile.id }, function (err, user) {  
+}, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
-    // });
   }
 ));
 
@@ -58,8 +64,7 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged out!!" )});
-
+// GitHub callback
 app.get('/github/callback', passport.authenticate('github', { 
     failureRedirect: '/api-docs', session: true }),
   (req, res) => {
@@ -68,11 +73,20 @@ app.get('/github/callback', passport.authenticate('github', {
     res.redirect('/');
   });
 
+// Logout route
+app.get('/', (req, res) => {
+  res.send(req.session.user ? `Logged in as ${req.session.user.displayName}` : "Logged out!!");
+});
+
+ 
+// Serve Swagger UI at /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } 
   else {
-    app.listen(port, () => {console.log(`Database is listening!, node is running too!! ${port}`);});
+    app.listen(port, () => {console.log(`🚀 Server running on port ${port}`);});
     }
 });
